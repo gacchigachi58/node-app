6bc82d83a50ffd366e7e2b3da065970e389bdfb6
Keitaro Koga <pokutin21@gmail.com>
2022-05-22 17:17:02 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { SrvRecord } from "dns"
 import { resolve } from "path"
 import { getDefaultSettings } from "http2"
 
+const modes = ['normal', 'hard'] as const
+type Mode = typeof modes[number]
+
 const sayHello = (name: string) => {
     return `Hello, ${name}!`
 }
@@ -12,26 +15,40 @@ const printLine = (text: string, breakLine: boolean = true) => {
     process.stdout.write(text + (breakLine ? '\n' : ''))
 }
 
+const readLine = async () => {
+    const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+    return input.trim()
+}
+
 const promptInput = async (text: string) => {
     printLine(`\n${text}\n> `, false)
-    const input: string = await new Promise((resolve) => process.stdout.once('data', (data) => resolve(data.toString())))
+    return readLine()
+}
 
-    return input.trim()
+const promptSelect = async <T extends string>(text: string, values: readonly T[]): Promise<T> => {
+    printLine(`\n${text}`)
+    values.forEach((value) => {
+        printLine(`- ${value}`)
+    })
+    printLine(`> `, false)
+
+    const input = await readLine() as T
+    if (values.includes(input)) {
+        return input
+    } else {
+        return promptSelect<T>(text, values)
+    }
 }
 
 class HitAndBlow {
     private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     private answer: string[] = []
     private tryCount = 0
+    private mode: Mode = 'normal'
 
-    // constructor() {
-    //     this.answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    //     this.answer = []
-    //     this.tryCount = 0
-    // }
-
-    setting() {
-        const answerLength = 3
+    async setting() {
+        this.mode = await promptSelect<Mode>('モードを入力してください', modes)
+        const answerLength = this.getAnswerLength()
 
         while (this.answer.length < answerLength) {
             const randNum = Math.floor(Math.random() * this.answerSource.length)
@@ -43,7 +60,14 @@ class HitAndBlow {
     }
 
     async play() {
-        const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+        const answerLength = this.getAnswerLength()
+        const inputArr = (await promptInput(`「,」区切りで${answerLength}つの数字を入力してください`)).split(',')
+        if (!this.validate(inputArr)) {
+            printLine(`無効な入力です`)
+            await this.play()
+            return
+        }
+        
         const result = this.check(inputArr)
 
         if (result.hit != this.answer.length) {
@@ -79,6 +103,24 @@ class HitAndBlow {
         process.exit()
     }
 
+    private validate(inputArr: string[]) {
+        const isLengthValid = inputArr.length == this.answer.length
+        const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
+        const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) == i)
+        return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
+    }
+
+    private getAnswerLength() {
+        switch (this.mode) {
+            case 'normal':
+                return 3
+            case 'hard':
+                return 4
+            default:
+                throw new Error(`${this.mode}は無効なモードです。`)
+        }
+    }
+
 }
 
     ; (async () => {
@@ -88,7 +130,7 @@ class HitAndBlow {
         // console.log(age)
         // process.exit()
         const hitAndBlow = new HitAndBlow()
-        hitAndBlow.setting()
+        await hitAndBlow.setting()
         await hitAndBlow.play()
         hitAndBlow.end()
     })()
